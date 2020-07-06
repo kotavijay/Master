28e779e7340961e422d6a617fb62e6d93dc8baf2
kotas <kotavj1234@gmail.com>
2020-07-06 19:54:39 +0530

    commit

---
+++ src/session6/inheritance/Animal.java
@@ -0,0 +1,43 @@
+package session6.inheritance;
+
+public class Animal {
+    private final String name;
+    private final int brain;
+    private final int body;
+    private final int size;
+    private final int weight;
+
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public void eat() {
+    }
+
+    public void move() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
---
+++ src/session6/inheritance/Dog.java
@@ -0,0 +1,39 @@
+package session6.inheritance;
+
+public class Dog extends Animal {
+
+    private final int eyes;
+    private final int legs;
+    private final int tail;
+    private final int teeth;
+    private final String coat;
+
+    public Dog(String name, int brain, int body, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+
+    public int getTeeth() {
+        return teeth;
+    }
+
+    public String getCoat() {
+        return coat;
+    }
+}
