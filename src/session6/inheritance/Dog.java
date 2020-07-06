9b749d828a948c9f1195a239e889361891dd451b
kotas <kotavj1234@gmail.com>
2020-07-06 20:04:11 +0530

    commit

--- src/session6/inheritance/Dog.java
+++ src/session6/inheritance/Dog.java
@@ -8,13 +8,17 @@ public class Dog extends Animal {
     private final int teeth;
     private final String coat;
 
-    public Dog(String name, int brain, int body, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
         super(name, 1, 1, size, weight);
         this.eyes = eyes;
         this.legs = legs;
         this.tail = tail;
         this.teeth = teeth;
         this.coat = coat;
+    }
+
+    public void chew() {
+        System.out.println("Dog.chew() called");
     }
 
     public int getEyes() {
--- src/session6/inheritance/Main.java
+++ src/session6/inheritance/Main.java
@@ -1,4 +1,12 @@
 package session6.inheritance;
 
 public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1, 1, 5, 10);
+
+        Dog dog = new Dog("Golden Retriever", 1, 58, 2, 4, 1, 20, "Long silky");
+
+        dog.eat();
+
+    }
 }
