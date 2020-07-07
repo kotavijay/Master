039b79d084ac15ba0b914d87ae0b4a99bf27ee3e
kotas <kotavj1234@gmail.com>
2020-07-07 13:19:45 +0530

    commit

--- src/session6/referenceobjectinstanceclass/Main.java
+++ src/session6/referenceobjectinstanceclass/Main.java
@@ -4,26 +4,53 @@ public class Main {
     public static void main(String[] args) {
 
 //        Creating instance of House
-        House house = new House("blue");
+        House blue = new House("blue");
 
-//        Passing reference of house variable to anotherHouse variable
-        House anotherHouse = house;
+//        Passing reference of blue variable to yellow variable
+        House yellow = blue;
 
         System.out.println("\n/********************/\n");
 
-        System.out.println(house.getColor());
-        System.out.println(anotherHouse.getColor());
+        System.out.println(blue.getColor());
+        System.out.println(yellow.getColor());
 
-//        Changing value of anotherHouse will change the value of house as anotherHouse is referring to house variable
+//        Changing value of yellow will change the value of blue as yellow is referring to blue variable
 //        Their physical value will be same
-        anotherHouse.setColor("yellow");
+        yellow.setColor("yellow");
 
         System.out.println("\n/********************/\n");
 
-        System.out.println(house.getColor());
-        System.out.println(anotherHouse.getColor());
+        System.out.println(blue.getColor());
+        System.out.println(yellow.getColor());
+
+
+//        Creating instance of House
+        House green = new House("green");
+
+//        Passing reference of green variable to red variable
+        House red = green;
 
         System.out.println("\n/********************/\n");
+
+        System.out.println(green.getColor());
+        System.out.println(red.getColor());
+
+//        Changing value of red will change the value of green as red is referring to green variable
+//        Their physical value will be same
+        red.setColor("red");
+
+//        Two different objects/instances has been created and created 2 reference
+//        Changing the reference will change the value of the its own object/instance and doesnt effect other instances/objects
+        System.out.println("\n/********************/\n");
+
+        System.out.println(blue.getColor());
+        System.out.println(yellow.getColor());
+        System.out.println();
+        System.out.println(green.getColor());
+        System.out.println(red.getColor());
+
+        System.out.println("\n/********************/\n");
+
 
     }
 }
